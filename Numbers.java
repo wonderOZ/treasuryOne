7ce4d28af9e9cba91ff76258d7d63431cc357a16
wonderOZ <green_dream@ukr.net>
2017-07-14 11:19:34 +0300

    Numbers.java
    Input five-digit number from keyboard. Receive column of digits.

--- Numbers.java
+++ Numbers.java
@@ -2,7 +2,7 @@ package net.ukr.green_dream;
 
 import java.util.Scanner;
 
-public class Main {
+public class Numbers {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
