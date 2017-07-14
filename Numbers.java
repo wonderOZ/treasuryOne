b01ea08a62514030e8db7a462323cc0cf53d51d8
wonderOZ <green_dream@ukr.net>
2017-07-14 11:25:13 +0300

    Update Numbers.java

--- Numbers.java
+++ Numbers.java
@@ -9,11 +9,11 @@ public class Numbers {
 				
 		System.out.print("Input 5-digit number and press Enter ");
 		int number = sc.nextInt();
-		int digitOne=number/10000;
-		int digitTwo=number%10000/1000;
-		int digitThree=number%10000%1000/100;
-		int digitFour=number%10000%1000%100/10;
-		int digitFive=number%10000%1000%100%10;
+		int digitOne = number / 10000;
+		int digitTwo = number % 10000 / 1000;
+		int digitThree = number % 10000 % 1000 / 100;
+		int digitFour = number % 10000 % 1000 % 100 / 10;
+		int digitFive = number % 10000 % 1000 % 100 % 10;
 		
 		System.out.println(digitOne);
 		System.out.println(digitTwo);
